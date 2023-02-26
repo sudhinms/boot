0faf7dbdb0e450a060addbbd22099368c11133c5
sudhinms <99173090+sudhinms@users.noreply.github.com>
2023-02-26 22:28:57 +0530

    Create CustomerModel.java

---
+++ model/CustomerModel.java
@@ -0,0 +1,53 @@
+package com.app.model;
+
+public class CustomerModel 
+{
+	private String customername;
+	private String email;
+	private Long mobile;
+	private String address;
+	private String password;
+	private String cpassword;
+	
+	
+	public Long getMobile() {
+		return mobile;
+	}
+	public void setMobile(Long mobile) {
+		this.mobile = mobile;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCustomername() {
+		return customername;
+	}
+	public void setCustomername(String customername) {
+		this.customername = customername;
+	}
+	public String getCpassword() {
+		return cpassword;
+	}
+	public void setCpassword(String cpassword) {
+		this.cpassword = cpassword;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+}
